358006c3d4a737fcf33dbcc82fe336e2af9dd6ae
Nicole Paz <nicoliveeira@gmail.com>
2024-11-29 16:49:33 -0300

    widget input field

---
+++ lib/common/widgets/input_field.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class InputField extends StatefulWidget {
+  final String label;
+  final String hintText;
+  final TextEditingController? controller;
+  final bool isPassword; 
+
+  const InputField({
+    super.key,
+    required this.label,
+    required this.hintText,
+    this.controller,
+    this.isPassword = false, 
+  });
+
+  @override
+  State<InputField> createState() => _InputFieldState();
+}
+
+class _InputFieldState extends State<InputField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.label,
+          style: const TextStyle(
+            color: Colors.black54,
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          ),
+        ),
+        const SizedBox(height: 5),
+        TextFormField(
+          controller: widget.controller,
+          obscureText: widget.isPassword ? _obscureText : false,
+          decoration: InputDecoration(
+            hintText: widget.hintText,
+            hintStyle: const TextStyle(color: Colors.black38),
+            suffixIcon: widget.isPassword
+                ? IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.black54,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  )
+                : null,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(color: Colors.black26),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(color: Colors.black26),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
