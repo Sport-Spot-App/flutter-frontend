9c941be55b2d3ee7d85cc1331ac7634bb8597d2a
Nicole Paz <nicoliveeira@gmail.com>
2024-11-29 16:49:51 -0300

    widget primary button

---
+++ lib/common/widgets/primary_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/common/constants/app_colors.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Gradient gradient;
+  final double width;
+  final double height;
+
+  const PrimaryButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.gradient = const LinearGradient(
+      colors: AppColors.orangeGradient,
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    ),
+    this.width = 300,
+    this.height = 50,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        gradient: gradient,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
