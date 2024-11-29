08830723ad518f8eaf207f154b366a42aa366499
Nicole Paz <nicoliveeira@gmail.com>
2024-11-29 16:51:39 -0300

    widget role button

---
+++ lib/common/widgets/role_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class RoleButton extends StatelessWidget {
+  final String title;
+  final String imagePath;
+  final VoidCallback onPressed;
+
+  const RoleButton({
+    super.key,
+    required this.title,
+    required this.imagePath,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFF6F0),
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              imagePath,
+              height: 120,
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF333333),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
