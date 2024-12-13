4a52d1a74e3854bbb530d1d96edfbb77b76fb289
Nicole Paz <nicoliveeira@gmail.com>
2024-12-13 13:51:20 -0300

    add toMap fuction

--- lib/models/auth_model.dart
+++ lib/models/auth_model.dart
@@ -4,8 +4,10 @@ class AuthModel {
   AuthModel({required this.token});
 
   factory AuthModel.fromMap(Map<String, dynamic> map) {
-    return AuthModel(
-      token: map['token'],
-    );
+    return AuthModel(token: map['token']);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {'token': token};
   }
 }
