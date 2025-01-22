16b06a4db89a2bcd436e6ca621c4a97ef40f97a4
Allan Gabriel <107281173+agp531@users.noreply.github.com>
2025-01-22 20:52:55 -0300

    Class to save data user logged

---
+++ lib/common/utils/user_map.dart
@@ -0,0 +1,41 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sport_spot/models/user_model.dart';
+
+class UserMap {
+  static Future setUserMap(UserModel user) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('userId', user.id);
+    prefs.setString('userName', user.name);
+    prefs.setString('userEmail', user.email);
+    prefs.setInt('urlRole', user.role);
+    prefs.setString('userPhoto', user.photo ?? "");
+    prefs.setString('userCellphone', user.cellphone);
+    prefs.setString('userDocument', user.document);
+    prefs.setBool('userApproved', user.is_approved);
+    prefs.setBool('userStatus', user.status);
+    prefs.setString("userCreateAt", user.created_at.toString());
+    prefs.setString("userUpdateAt", user.updated_at.toString());
+  }
+
+  static Future<UserModel> getUserMap() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    var auxCreate = prefs.getString("userCreateAt");
+    DateTime createAt = auxCreate == null ? DateTime.now() : DateTime.parse(auxCreate);
+    var auxUpdate = prefs.getString("userCreateAt");
+    DateTime updateAt = auxUpdate == null ? DateTime.now() : DateTime.parse(auxUpdate);
+
+    return UserModel(
+      id: prefs.getInt('userId') ?? 0,
+      name: prefs.getString('userName') ?? "",
+      email: prefs.getString('userEmail') ?? "",
+      role: prefs.getInt('userRole') ?? 0,
+      photo: prefs.getString('userPhoto') ?? "",
+      cellphone: prefs.getString('userCellphone') ?? "",
+      document: prefs.getString('userDocument') ?? "",
+      is_approved: prefs.getBool('userApproved') ?? false,
+      status: prefs.getBool('userStatus') ?? true,
+      created_at: createAt,
+      updated_at: updateAt,
+    );
+  }
+}
