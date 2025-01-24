fd3ab7e4a2c6cfc9f644c30a6bb0fd466ed619cb
Nicole Paz <nicoliveeira@gmail.com>
2025-01-24 17:28:12 -0300

    Add SearchField widget

---
+++ lib/common/widgets/search_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:sport_spot/common/constants/app_colors.dart';
+
+class SearchField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String hintText;
+  final ValueChanged<String>? onChanged;
+
+  const SearchField({
+    Key? key,
+    this.controller,
+    this.hintText = "Pesquisar",
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        border: Border.all(
+          color: AppColors.mediumOrange,
+          width: 2,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            offset: const Offset(0, 3),
+            blurRadius: 6,
+          ),
+        ],
+      ),
+      child: TextField(
+        controller: controller,
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          prefixIcon: const Icon(
+            Icons.search,
+            color: Colors.black,
+          ),
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            color: Colors.black54,
+          ),
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        ),
+      ),
+    );
+  }
+}
