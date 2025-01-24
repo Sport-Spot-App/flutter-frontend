11035ab8b2cc01e61ceef0264c3b16f3eeb1f681
Nicole Paz <nicoliveeira@gmail.com>
2025-01-24 17:27:21 -0300

    Add green color constant to AppColors

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -7,6 +7,7 @@ class AppColors {
   static const Color darkOrange = Color(0xFFF85B00);
 
   static const Color gray = Color(0xFF666666);
+  static const Color green = Color(0XFF438883);
   static const Color charcoalBlue = Color(0XFF37474F);
   
   static const List<Color> orangeGradient = [Color(0xFFF85B00),Color(0xFFEF9A61)];
