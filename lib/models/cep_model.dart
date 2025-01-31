6f6987265338a4ebff03dac7af3b3d3fdd673632
Allan Gabriel <107281173+agp531@users.noreply.github.com>
2025-01-31 20:06:10 -0300

    CEP model

--- lib/models/cep_model.dart
+++ lib/models/cep_model.dart
@@ -1,23 +1,67 @@
 class CepModel {
-  final int id;
-  final String name;
+  final String cep;
+  final String logradouro;
+  final String complemento;
+  final String unidade;
+  final String bairro;
+  final String localidade;
+  final String uf;
+  final String estado;
+  final String regiao;
+  final String ibge;
+  final String gia;
+  final String ddd;
+  final String siafi;
 
   CepModel({
-    required this.id,
-    required this.name,
+    required this.cep,
+    required this.logradouro,
+    required this.complemento,
+    required this.unidade,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+    required this.estado,
+    required this.regiao,
+    required this.ibge,
+    required this.gia,
+    required this.ddd,
+    required this.siafi,
   });
 
   factory CepModel.fromMap(Map<String, dynamic> map) {
     return CepModel(
-      id: map['id'],
-      name: map['name'],
+      cep: map['cep'],
+      logradouro: map['logradouro'],
+      complemento: map['complemento'],
+      unidade: map['unidade'],
+      bairro: map['bairro'],
+      localidade: map['localidade'],
+      uf: map['uf'],
+      estado: map['estado'],
+      regiao: map['regiao'],
+      ibge: map['ibge'],
+      gia: map['gia'],
+      ddd: map['ddd'],
+      siafi: map['siafi'],
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
-      'name': name,
+      "cep": cep,
+      "logradouro": logradouro,
+      "complemento": complemento,
+      "unidade": unidade,
+      "bairro": bairro,
+      "localidade": localidade,
+      "uf": uf,
+      "estado": estado,
+      "regiao": regiao,
+      "ibge": ibge,
+      "gia": gia,
+      "ddd": ddd,
+      "siafi": siafi,
     };
   }
 }
