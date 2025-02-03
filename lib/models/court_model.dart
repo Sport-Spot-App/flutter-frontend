cdf8a418d94c7a544bfd69e48e991f24d32f2f18
Nicole Paz <nicoliveeira@gmail.com>
2025-02-02 22:23:40 -0300

    refactor: update CourtModel to support nullable fields and add schedules

--- lib/models/court_model.dart
+++ lib/models/court_model.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:sport_spot/models/cep_model.dart';
+import 'package:sport_spot/models/court_schedules_model.dart';
 
 class CourtModel {
-  final int id;
+  final int? id;
   final String name;
   final String description;
   final String zip_code;
@@ -10,16 +13,17 @@ class CourtModel {
   final String number;
   final String? coordinate_x;
   final String? coordinate_y;
-  final int user_id;
-  final DateTime created_at;
-  final DateTime updated_at;
+  final int? user_id;
+  final DateTime? created_at;
+  final DateTime? updated_at;
   final DateTime? deleted_at;
-  final List<String> sports;
-  final List<String> photos;
-  final CepModel cep;
+  final List<int> sports;
+  final List<File>? photos;
+  final CepModel? cep;
+  final List<CourtSchedulesModel> schedules;
 
   CourtModel({
-    required this.id,
+    this.id,
     required this.name,
     required this.description,
     required this.zip_code,
@@ -28,13 +32,14 @@ class CourtModel {
     required this.number,
     this.coordinate_x,
     this.coordinate_y,
-    required this.user_id,
-    required this.created_at,
-    required this.updated_at,
+    this.user_id,
+    this.created_at,
+    this.updated_at,
     this.deleted_at,
     required this.sports,
-    required this.photos,
-    required this.cep,
+    this.photos,
+    this.cep,
+    required this.schedules,
   });
 
   factory CourtModel.fromMap(Map<String, dynamic> map) {
@@ -53,9 +58,13 @@ class CourtModel {
       updated_at: DateTime.parse(map['updated_at']),
       deleted_at:
           map['deleted_at'] != null ? DateTime.parse(map['deleted_at']) : null,
-      sports: List<String>.from(map['sports'] ?? []),
-      photos: List<String>.from(map['photos'] ?? []),
-      cep: CepModel.fromMap(map),
+      sports: List<int>.from(map['sports'] ?? []),
+      photos: (map['photos'] as List<dynamic>?)
+          ?.map((item) => File(item['path']))
+          .toList(),
+      cep: map['cep'] != null ? CepModel.fromMap(map['cep']) : null,
+      schedules: List<CourtSchedulesModel>.from(
+          map['schedules']?.map((x) => CourtSchedulesModel.fromMap(x)) ?? []),
     );
   }
 
@@ -71,12 +80,13 @@ class CourtModel {
       'coordinate_x': coordinate_x,
       'coordinate_y': coordinate_y,
       'user_id': user_id,
-      'created_at': created_at.toIso8601String(),
-      'updated_at': updated_at.toIso8601String(),
+      'created_at': created_at?.toIso8601String(),
+      'updated_at': updated_at?.toIso8601String(),
       'deleted_at': deleted_at?.toIso8601String(),
       'sports': sports,
-      'photos': photos,
-      'cep': cep.toMap(),
+      'photos': photos?.map((file) => {'path': file.path}).toList(),
+      'cep': cep?.toMap(),
+      'schedules': schedules.map((x) => x.toMap()).toList(),
     };
   }
 }
