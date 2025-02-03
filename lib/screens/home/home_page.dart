921bf881dda64d61ac5411831100d8ef1c3339da
Nicole Paz <nicoliveeira@gmail.com>
2025-02-03 14:29:45 -0300

    fix: refactor court image URL handling and update sports type display

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -30,11 +30,11 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _fetchCourts() async {
-    await courtStore.getCourts();
-    setState(() {
-      filteredCourts = courtStore.state.value;
-      isLoading = false;
-    });
+      await courtStore.getCourts();
+      setState(() {
+        filteredCourts = courtStore.state.value;
+        isLoading = false;
+      });
   }
 
   Future<void> _fetchFavoriteCourts() async {
@@ -129,11 +129,20 @@ class _HomePageState extends State<HomePage> {
                             .pushNamed(viewCourt, arguments: court);
                       },
                       child: CourtCard(
-                        imageUrlList:
-                            court.photos?.map((file) => file.path).toList() ??
-                                [],
+                        imageUrlList: 
+                        // court.photos?.map((file) {
+                        //   final path = file.path;
+                        //   if (path.startsWith('http')) {
+                        //     return path;
+                        //   } else if (path.startsWith('/')) {
+                        //     return 'https://sportspott.tech$path'; // Replace with your server URL
+                        //   } else {
+                        //     return 'https://sportspott.tech/$path'; // Replace with your server URL
+                        //   }
+                        // }).toList() ?? 
+                        [],
                         name: court.name,
-                        type: court.sports.join(', '),
+                        type: court.sports.map((sport) => sport.name).join(', '),
                         price: court.price_per_hour,
                         favoriteIcon: IconButton(
                           icon: Icon(
