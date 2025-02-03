f6d539e09c302c1e1cba303e41a99db4770343a9
Nicole Paz <nicoliveeira@gmail.com>
2025-02-02 22:12:01 -0300

    ensure null safety for court photos

--- lib/common/widgets/court_owner_card.dart
+++ lib/common/widgets/court_owner_card.dart
@@ -6,7 +6,7 @@ class CourtOwnerCard extends StatefulWidget {
   final CourtModel court;
 
   const CourtOwnerCard(this.court, {super.key});
-  
+
   @override
   State<CourtOwnerCard> createState() => _CourtOwnerCardState();
 }
@@ -20,14 +20,15 @@ class _CourtOwnerCardState extends State<CourtOwnerCard> {
       child: ListTile(
         leading: ClipRRect(
           borderRadius: const BorderRadius.all(Radius.circular(5)),
-          child: widget.court.photos.isNotEmpty
-              ? Image.network(widget.court.photos[0])
+          child: widget.court.photos!.isNotEmpty
+              ? Image.network(widget.court.photos![0].path)
               : const Icon(Icons.image_not_supported),
         ),
         title: Center(
           child: Text(widget.court.name),
         ),
-        titleTextStyle: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        titleTextStyle:
+            const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         subtitle: Column(
           children: [
             Text(widget.court.sports.join(', ')),
@@ -36,7 +37,8 @@ class _CourtOwnerCardState extends State<CourtOwnerCard> {
           ],
         ),
         onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (_) => CreateCourtPage(court: widget.court)));
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (_) => CreateCourtPage(court: widget.court)));
         },
       ),
     );
