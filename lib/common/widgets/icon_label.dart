59ecc2690675597f0c6cfe5f8f147558408adf5d
Nicole Paz <nicoliveeira@gmail.com>
2025-02-04 10:35:20 -0300

    Enhance IconWithLabel widget with selection state and tap handling

--- lib/common/widgets/icon_label.dart
+++ lib/common/widgets/icon_label.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:sport_spot/common/constants/app_colors.dart';
 
 class IconWithLabel extends StatelessWidget {
   final IconData icon;
   final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
 
-  const IconWithLabel({required this.icon, required this.label, super.key});
+  const IconWithLabel({
+    required this.icon,
+    required this.label,
+    required this.isSelected,
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        children: [
-          Icon(icon, color: Colors.grey),
-          const SizedBox(height: 4),
-          Text(label, style: const TextStyle(color: Colors.grey)),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          children: [
+            Icon(
+              icon,
+              color: isSelected ? AppColors.darkOrange : const Color.fromARGB(133, 0, 0, 0),
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                color: isSelected ? AppColors.darkOrange : const Color.fromARGB(133, 0, 0, 0),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
