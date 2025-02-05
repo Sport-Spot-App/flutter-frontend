3a1d2a019e27fd1a254c68d61632566706a5f141
Nicole Paz <nicoliveeira@gmail.com>
2025-02-05 17:32:30 -0300

    Add options to CourtOwnerCard for blocking hours and editing court details

--- lib/common/widgets/court_owner_card.dart
+++ lib/common/widgets/court_owner_card.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:sport_spot/api/api.dart';
 import 'package:sport_spot/models/court_model.dart';
+import 'package:sport_spot/repositories/court_repository.dart';
+import 'package:sport_spot/screens/court/court_booking.dart';
 import 'package:sport_spot/screens/court/create_court_page.dart';
+import 'package:dio/dio.dart';
+import 'package:sport_spot/stores/court_store.dart';
 
 class CourtOwnerCard extends StatefulWidget {
   final CourtModel court;
+  final VoidCallback onDelete;
 
-  const CourtOwnerCard(this.court, {super.key});
+  const CourtOwnerCard(this.court, {required this.onDelete, super.key});
 
   @override
   State<CourtOwnerCard> createState() => _CourtOwnerCardState();
@@ -42,8 +48,34 @@ class _CourtOwnerCardState extends State<CourtOwnerCard> {
           ],
         ),
         onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (_) => CreateCourtPage(court: widget.court)));
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.block),
+                    title: const Text("Bloquear Horários"),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (_) => CourtBookingPage(court: widget.court)));
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.edit),
+                    title: const Text("Editar Quadra"),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (_) => CreateCourtPage(court: widget.court)));
+                    },
+                  ),
+                ],
+              );
+            },
+          );
         },
       ),
     );
